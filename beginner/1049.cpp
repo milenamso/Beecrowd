1949952d69c251c110f843da353f86f03283f91b
Milena Marques <84979688+milenamso@users.noreply.github.com>
2025-09-21 15:05:01 -0300

    solution

---
+++ beginner/1049.cpp
@@ -0,0 +1,39 @@
+/*
+In this problem, the point is read three Portuguese words. 
+These words define an animal according to the table below, from left to right. 
+After, print the chosen animal defined by these three words.
+
+i used the function tolower to turned any letters in my string in lowercase
+and i used the RANGE-BASED FOR LOOP C++ to implement the lowercase idea
+
+the comlexity is O(1) → constant time, independent of input size (since you always get exactly 3 small words).
+Memory: O(1) → only three short strings and some references.
+*/
+
+
+#include <bits/stdc++.h>
+
+using namespace std;
+ 
+int main() {
+ 
+    string a, b, c;
+    
+    cin >> a >> b >> c;
+    
+    for (char &ch : a) ch = tolower(ch);
+    for (char &ch : b) ch = tolower(ch);
+    for (char &ch : c) ch = tolower(ch);
+    
+    if(a == "vertebrado" && b == "ave" && c == "carnivoro")             cout << "aguia" << endl;
+    else if(a == "vertebrado" && b == "ave" && c == "onivoro")          cout << "pomba" << endl;
+    else if(a == "vertebrado" && b == "mamifero" && c == "onivoro")     cout << "homem" << endl;
+    else if(a == "vertebrado" && b == "mamifero" && c == "herbivoro")   cout << "vaca" << endl;
+    
+    else if(a == "invertebrado" && b == "inseto" && c == "hematofago")  cout << "pulga" << endl;
+    else if(a == "invertebrado" && b == "inseto" && c == "herbivoro")   cout << "lagarta" << endl;
+    else if(a == "invertebrado" && b == "anelideo" && c == "hematofago")cout << "sanguessuga" << endl;
+    else if(a == "invertebrado" && b == "anelideo" && c == "onivoro")  cout << "minhoca" << endl; 
+    
+    return 0;
+}
